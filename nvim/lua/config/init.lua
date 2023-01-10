91a917c25f4dfb6ac0c081a467a2cc11efe90134
Robert Craigie <robert@craigie.dev>
2023-01-10 22:33:15 +0000

    Neovim: add kotlin treesitter

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -312,7 +312,7 @@ require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
   ensure_installed = {
     'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help',
-    'graphql', 'prisma',
+    'graphql', 'prisma', 'kotlin',
   },
 
   highlight = { enable = true },
