c28da2dfc0a5a4dfe39e9e959fe13d5473a45b71
Robert Craigie <robert@craigie.dev>
2023-06-04 22:12:23 +0100

    Neovim: add yank/past keybindings for system clipboard

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -15,3 +15,15 @@ vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 vim.keymap.set("n", "<leader>cn", function()
   require("notify").dismiss()
 end, { desc = "Clear nvim-notify notifications" })
+
+-- Yank into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", '"+y') -- yank motion
+vim.keymap.set({ "n", "v" }, "<leader>Y", '"+Y') -- yank line
+
+-- Delete into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>d", '"+d') -- delete motion
+vim.keymap.set({ "n", "v" }, "<leader>D", '"+D') -- delete line
+
+-- Paste from system clipboard
+vim.keymap.set("n", "<leader>p", '"+p') -- paste after cursor
+vim.keymap.set("n", "<leader>P", '"+P') -- paste before cursor
