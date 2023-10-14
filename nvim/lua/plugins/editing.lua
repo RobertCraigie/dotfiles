17cd026d3693966bda71c7c4bce55894d9f1e9d1
Robert Craigie <robert@craigie.dev>
2023-10-14 12:52:10 +0100

    Neovim: bump plugins

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -64,9 +64,12 @@ return {
   -- Add indentation guides even on blank lines
   {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     opts = {
-      char = "┊",
-      show_trailing_blankline_indent = false,
+      indent = {
+        char = "┊",
+      },
+      -- show_trailing_blankline_indent = false,
     },
   },
 
