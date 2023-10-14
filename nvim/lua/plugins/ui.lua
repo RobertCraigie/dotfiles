a66b91c99e0e10b68eba8599d2e164f2202db2bf
Robert Craigie <robert@craigie.dev>
2023-10-14 12:43:42 +0100

    Neovim: remove unused neo-tree plugin

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,26 +1,4 @@
 return {
-  -- File tree
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-    },
-    keys = {
-      { "<leader>ft", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
-    },
-    config = function()
-      vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-      require("neo-tree").setup()
-      require("nvim-web-devicons").setup({
-        -- globally enable default icons (default to false)
-        -- will get overriden by `get_icons` option
-        default = true,
-      })
-    end,
-  },
-
   {
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
