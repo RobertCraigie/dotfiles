1cece5bc3e756f3e62411c58da7cd3973f48ccaa
Robert Craigie <robert@craigie.dev>
2025-03-29 17:39:52 +0000

    bootstrap updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -23,6 +23,7 @@ return {
         "markdown",
         "markdown_inline",
         "typescript",
+        "javascript",
         "graphql",
         "prisma",
         "kotlin",
--- profile
+++ profile
@@ -1,5 +1,3 @@
 . "$HOME/.cargo/env"
 
-export PATH="$HOME/.poetry/bin:$PATH"
-
 . "$HOME/.atuin/bin/env"
