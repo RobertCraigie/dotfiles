785062364f1cf294ed711c6780b831fbe50dca0e
Robert Craigie <robert@craigie.dev>
2025-09-02 10:19:40 +0100

    nvim: update lsp mappings

--- nvim/lua/config/util/lsp.lua
+++ nvim/lua/config/util/lsp.lua
@@ -33,14 +33,14 @@ M.on_attach = function(client, bufnr)
   end
 
   nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-  nmap("<leader>Ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  nmap("ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
   nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
   nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
   nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
   nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
 
-  nmap("ds", function()
+  nmap("<ldeader>ds", function()
     require("telescope.builtin").lsp_document_symbols()
   end, "[D]ocument [S]ymbols")
   nmap("gm", function()
