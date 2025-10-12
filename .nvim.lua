56bab6a3f7a8b93ed887d456066309dfc8f8c657
Robert Craigie <robert@craigie.dev>
2025-10-12 16:05:53 -0400

    meta: switch to dprint

---
+++ .nvim.lua
@@ -0,0 +1,22 @@
+local Util = require("config.util.lsp")
+
+vim.lsp.config('dprint', {
+  cmd = { "pnpm", "dprint", "lsp" },
+  on_attach = Util.on_attach,
+})
+vim.lsp.enable('dprint')
+
+vim.lsp.enable('prettier', false)
+vim.lsp.enable('prettierd', false)
+
+require('conform').setup({
+  log_level = vim.log.levels.DEBUG,
+  formatters_by_ft = {
+    javascript = { "dprint" },
+    typescript = { "dprint" },
+    css = { "dprint" },
+    markdown = { "dprint" },
+    jsonc = { "dprint" },
+    html = { "dprint" },
+  },
+})
