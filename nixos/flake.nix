898994b685bac61289fd540424a2dc44d903a235
Robert Craigige <robert@craigie.dev>
2026-05-13 18:52:24 +0100

    init

---
+++ nixos/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
