95b3de7240a12f440688a75ab1809c0898d70266
Robert Craigige <robert@craigie.dev>
2026-05-14 18:01:26 +0100

    cleanup

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -178,7 +178,7 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
       user = "greeter";
     };
   };
